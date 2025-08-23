5b79efb54cba375ebf0a4543caaf12d74ed83d99
xxxNdr <andrea1987andre@gmail.com>
2025-08-23 20:02:15 +0200

    rinomina cartelle

--- esercizi_estate/es10/inc/ctrl.php
+++ esercizi_estate/es10/inc/ctrl.php
@@ -46,19 +46,19 @@ $maratonaCine3 = [
 
 $risultato = '';
 
-if(isset($_POST['gen'])){
-    $fav = explode(',',$_POST['gen']);
+if (isset($_POST['gen'])) {
+    $fav = explode(',', $_POST['gen']);
     $fav = array_map('trim', $fav);
     $fav = array_map('strtolower', $fav);
 
     $trovati = [];
-    foreach($maratonaCine3 as $f){
-        if(in_array(strtolower($f['genere']), $fav)){
+    foreach ($maratonaCine3 as $f) {
+        if (in_array(strtolower($f['genere']), $fav)) {
             $trovati[] = $f;
         }
     }
 
-    usort($trovati, function($a,$b){
+    usort($trovati, function ($a, $b) {
         return strcasecmp($a['orario'], $b['orario']);
         /*
             strcasecmp confronta 2 stringhe in modo case-insensitive e restituisce un valore:
@@ -69,12 +69,12 @@ if(isset($_POST['gen'])){
             */
     });
 
-    if(empty($trovati)){
+    if (empty($trovati)) {
         $risultato = "Nessun genere trovato per la tua scelta: " . implode(', ', $fav);
-    }else{
+    } else {
         $risultato = "<h3>Film disponibili:</h3>";
-        foreach($trovati as $f){
-            $risultato .= "<p><strong>{$f['titolo']}</strong><br> genere: {$f['genere']}, delle ore: {$f['orario']}</p>"; 
+        foreach ($trovati as $f) {
+            $risultato .= "<p><strong>{$f['titolo']}</strong><br> genere: {$f['genere']}, delle ore: {$f['orario']}</p>";
         }
     }
 };
